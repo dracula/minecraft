3b4261688413b29f09711ee5abc12c0f5f36b0ea
Xetheon <sushileaf@protonmail.com>
2023-11-26 20:42:25 -0600

    Reduce pack generation time by using inkscape shell mode and improve genscript as a whole

--- genscript.sh
+++ genscript.sh
@@ -4,7 +4,10 @@
 # Output located in ./generated
 
 # Pack version
-PACKVER=v"2.0 B2"
+PACKVER="Dev"
+
+# Pack formats to generate
+FORMATS=(20 18 15 13 12 9 8 7 6 5 4 3 2 1 0)
 
 # Minecraft version ranges for each pack format
 MCVER=(1.6.1-1.7.10 1.8-1.8.9 1.9–1.10.2 1.11–1.12.2 1.13–1.14.4 1.15–1.16.1 1.16.2–1.16.5 1.17–1.17.1 1.18–1.18.2 1.19–1.19.2 0 0 1.19.3 1.19.4 0 1.20–1.20.1 0 0 1.20.2 0 1.21)
@@ -12,8 +15,11 @@ MCVER=(1.6.1-1.7.10 1.8-1.8.9 1.9–1.10.2 1.11–1.12.2 1.13–1.14.4 1.15–1.
 # DPI range for GUI sizes
 DPI=(0 24 48 72 96 120 144 168 192)
 
+# GUI Scales to generate
+SCALES=(2 3 4 5 6 7 8)
+
 # Exit if running under root
-[ "$EUID" = 0 ] && {
+[ "${EUID}" = 0 ] && {
 
     echo
     echo ":: Please run without root privileges."
@@ -31,41 +37,52 @@ DPI=(0 24 48 72 96 120 144 168 192)
 }
 
 # Check deps
+MISSING=0
 hash inkscape 2>/dev/null || {
 
     echo "Inkscape is not installed."
-    EXIT=1
+    MISSING=$((${MISSING}+1))
 }
 hash 7z 2>/dev/null || {
 
     echo "7z (7-Zip, p7zip) is not installed."
-    EXIT=1
-}
-hash sed 2>/dev/null || {
-
-    echo "sed is not installed."
-    EXIT=1
+    MISSING=$((${MISSING}+1))
 }
 hash mogrify 2>/dev/null || {
 
     echo "imagemagick is not installed."
-    EXIT=1
+    MISSING=$((${MISSING}+1))
 }
 hash optipng 2>/dev/null || {
 
     echo "optipng is not installed."
-    EXIT=1
+    MISSING=$((${MISSING}+1))
 }
-[ "$EXIT" = "1" ] && {
+
+# Exit if dependencies aren't met
+[ "${MISSING}" -gt "0" ] && {
 
     echo
-    echo ":: One or more dependencies are missing. Exiting."
+    echo ":: ${MISSING} dependencies are missing. Exiting."
     echo
     exit 1
 }
 
+# Check if ./temp directory exists
+if [ -d ./temp ]; then
+
+    while [ "$INPUT" != "y" ]; do
+
+        echo
+        echo ":: ./temp directory already exists (script error?). Remove? [y/n]"
+        read -s -n 1 INPUT
+        [ "$INPUT" = "n" ] && exit 1
+        [ "$INPUT" = "y" ] && \rm -r ./temp
+    done
+fi
+
 # Check if generated packs already exist
-if [ -d "./generated" ]; then
+if [ -d ./generated ]; then
 
     while [ "$INPUT" != "y" ]; do
 
@@ -77,55 +94,64 @@ if [ -d "./generated" ]; then
     done
 fi
 
+echo ":: Starting generation (this may take a while!)"
+
 # Enable globstar during script
 shopt -s globstar
 
 # Generate useable PNGs
-echo ":: Starting generation (this may take a while!)"
 mkdir ./temp
-for SCALE in 2 3 4 5 6 7 8; do
+for SCALE in ${SCALES[@]}; do
 
-    echo "Generating for scale $SCALE..."
-    cp -r ./src ./temp/$SCALE
-    for SVG in ./temp/$SCALE/**/*.svg; do
-        inkscape "$SVG" -d ${DPI[$SCALE]} --export-filename "${SVG%svg}png"
+    echo "Generating images for scale ${SCALE}..."
+    rsync -a --exclude="*.svg" ./src/ ./temp/${SCALE}
+    for SVG in ./src/**/*.svg; do
+
+        PNG=$(sed "s~src/~temp/$SCALE/~" <<< ${SVG} | sed -e "s~.svg~.png~")
+        echo "file-open:${SVG}; export-filename:${PNG}; export-dpi:${DPI[$SCALE]}; export-do" >> ./temp/${SCALE}/inkscape.txt
+    done
+    echo "quit" >> ./temp/${SCALE}/inkscape.txt
+    inkscape --shell < ./temp/${SCALE}/inkscape.txt &>/dev/null
+done
+
+# Remove almost fully transparent pixels that don't play nice with Minecraft's texture rendering.
+for SCALE in ${SCALES[@]}; do
+
+    echo "Removing redundant pixels for scale $SCALE..."
+    for PNG in ./temp/$SCALE/**/*.png; do
+
+        mogrify -fuzz 7% -channel RGBA -fill "#ffffff" -transparent "#88888800" ${PNG}
     done
 done
 
-# Remove temporary SVGs
-for SVG in ./temp/**/*.svg; do
+# Compress generated PNG files
+for SCALE in ${SCALES[@]}; do
 
-    rm $SVG
+    echo "Optimizing images for scale $SCALE..."
+    for PNG in ./temp/$SCALE/**/*.png; do
+
+        optipng -q ${PNG}
+    done
 done
 
-# Compress PNGs
-echo "Optimizing images..."
-for PNG in ./temp/**/*.png; do
-
-    mogrify -fuzz 7% -channel RGBA -fill "#ffffff" -transparent "#88888800" $PNG
-    optipng $PNG
-done
 
 # Sort and compress files
 echo "Zipping packs..."
 mkdir ./generated
-for SCALE in 2 3 4 5 6 7 8; do
+for SCALE in ${SCALES[@]}; do
 
-    for FRMT in 20 18 15 13 12 9 8 7 6 5 4 3 2 1 0; do
+    sed -i "s/SCALE/${SCALE}/g" ./temp/${SCALE}/pack.mcmeta
+    for FORMAT in ${FORMATS[@]}; do
 
-        [ $SCALE = 2 ] && mkdir ./generated/$FRMT
-        sed -i -e "s/-FRMT/$FRMT/g" ./temp/$SCALE/pack.mcmeta
-        sed -i -e "s/-SCALE/$SCALE/g" ./temp/$SCALE/pack.mcmeta
-        [ "$FRMT" = 15 ] && \cp -r ./temp/$SCALE/1.20-1.20.1 ./temp/$SCALE/assets
-        [ "$FRMT" = 13 ] && \cp -r ./temp/$SCALE/1.19.3-1.19.4 ./temp/$SCALE/assets
-        [ "$FRMT" = 9 ] && \cp -r ./temp/$SCALE/1.9-1.19.2 ./temp/$SCALE/assets
-        [ "$FRMT" = 1 ] && \cp -r ./temp/$SCALE/1.8-1.8.9 ./temp/$SCALE/assets
-        [ "$FRMT" = 0 ] && {
-            sed -i -e "s/: 0/: 1/g" ./temp/$SCALE/pack.mcmeta
-            \cp -r ./temp/$SCALE/1.6.1-1.7.10 ./temp/$SCALE/assets
-        }
-        7z a "./generated/$FRMT/Dracula $PACKVER GUI Scale $SCALE MC ${MCVER[$FRMT]}.zip" ./temp/$SCALE/assets ./temp/$SCALE/LICENSE.md ./temp/$SCALE/pack.mcmeta ./temp/$SCALE/pack.png
-        sed -i -e "s/: $FRMT/: -FRMT/g" ./temp/$SCALE/pack.mcmeta
+        [ ${SCALE} = 2 ] && mkdir ./generated/${FORMAT}
+        sed -i "s/FORMAT/${FORMAT}/g" ./temp/${SCALE}/pack.mcmeta
+        if [ -d ./temp/${SCALE}/${FORMAT} ]; then
+
+            \cp -r ./temp/${SCALE}/${FORMAT}/* ./temp/${SCALE}/assets
+            [ "${FORMAT}" = 0 ] && sed -i "s/: 0/: 1/g" ./temp/${SCALE}/pack.mcmeta
+        fi
+        7z a "./generated/${FORMAT}/Dracula ${PACKVER} GUI Scale ${SCALE} MC ${MCVER[$FORMAT]}.zip" ./temp/${SCALE}/assets ./temp/${SCALE}/LICENSE.md ./temp/${SCALE}/pack.mcmeta ./temp/${SCALE}/pack.png &>/dev/null
+        sed -i "s/: ${FORMAT}/: FORMAT/g" ./temp/${SCALE}/pack.mcmeta
     done
 done
 
